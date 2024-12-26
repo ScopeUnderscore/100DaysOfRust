a3f92cc6fbf7f2d73eb9a9fe1c1e57586f0fee74
ScopeUnderscore <anandhupraj1279@gmail.com>
2024-12-26 19:29:45 +0530

    first day of 100 and first line of Rust

---
+++ helloworld.rs
@@ -0,0 +1,8 @@
+// this is a command , and is ignored by the compiler
+// This is the main function
+fn main(){
+    //Statement here are executed when the compiled binary is called 
+    // print text to the console
+    println!("Hello World!");
+    println!("I am a Rustacean!")
+}
